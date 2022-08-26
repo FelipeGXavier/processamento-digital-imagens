a35ab5c603a6f226f2e48fc3fbb311d5fa5a1a47
FelipeGXavier <felipexavier20015@gmail.com>
2022-08-26 10:29:31 -0300

    files: add more images

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import { PPM, PGM, createRandomImage } from "./randomImage.service";
 
-createRandomImage(PPM, 100, 100, {start: 0, end: 255}, "ppm_100_100");
+createRandomImage(PPM, 1000, 1000, {start: 0, end: 255}, "ppm_1000_1000");
 createRandomImage(PGM, 100, 100, {start: 0, end: 255}, "pgm_100_100");
